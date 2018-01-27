dd61cb6995250ea3a4ddadb7be3e276d8cf157a9
rajnishcoder <getwere@gmail.com>
2018-01-27 15:28:11 +0530

    improved open calculator command in openprogramm.py

--- OpenProgram.py
+++ OpenProgram.py
@@ -2,11 +2,16 @@ import os
 
 class OpenPregramClass(object):
     def __init__(self, fileName):
-        try:
-            os.system(fileName+'.exe')
-        except Exception as e:
-            print(e)
-        pass
+        # changing commands for specific programs
+        if 'calculator' in fileName:
+            fileName = 'calc'
+
+            # opening programs
+            try:
+                os.system(fileName+'.exe')
+            except Exception as e:
+                print(e)
+            pass
     
     # for other programs
     def openCustomPrograms(args):
