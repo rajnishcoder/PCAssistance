17c9c05dc3dd1940e0a3ecb6d1b5ca65ced08c4a
rajnishcoder <getwere@gmail.com>
2018-01-31 06:26:19 +0530

    opening program model update

--- models/ProgramModel.py
+++ models/ProgramModel.py
@@ -3,3 +3,4 @@ class ClassName(object):
     def __init__(self, *args):
         self.firefoxDev = 'C:\Program Files\Firefox Developer Edition\Firefox.exe'
         self.firexfox = 'C:\Program Files\Mozilla Firefox\Firefox.exe'
+        self.chrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
