7f71d7751ca8537fcbec98dbfcfe513de53a5955
rajnishcoder <getwere@gmail.com>
2018-02-04 18:44:02 +0530

    todays push

--- database/database.py
+++ database/database.py
@@ -1,7 +1,7 @@
 import MySQLdb
 
 # Open database connection
-db = MySQLdb.connect("localhost","testuser","test123","TESTDB" )
+db = MySQLdb.connect("localhost","testuser","","root" )
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
