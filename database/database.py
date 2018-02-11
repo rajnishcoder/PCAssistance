7286afbb354b37590d650fb6c046501412c0e899
rajnishcoder <getwere@gmail.com>
2018-02-11 18:43:50 +0530

    databasetest

--- database/database.py
+++ database/database.py
@@ -3,6 +3,6 @@ import MySQLdb
 db = MySQLdb.connect(host="localhost", user="root", passwd="")
 
 c = db.cursor()
-c.execute('create database if not exists pythontest')
+c.execute('create database if not exists')
 
 db.close()
