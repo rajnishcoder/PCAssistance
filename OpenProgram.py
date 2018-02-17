11a719b2b385aba7aa08d2fc9a1089d47d37769f
rajnishcoder <getwere@gmail.com>
2018-02-17 07:11:51 +0530

    added open cmd by subprocess

--- OpenProgram.py
+++ OpenProgram.py
@@ -1,19 +1,22 @@
 import os
-
+import subprocess
 
 class OpenProgramClass(object):
     def __init__(self, fileName):
         print('here')
         # changing commands for specific programs
-        if 'calculator' in fileName:
-            fileName = 'calc'
+        progName = fileName.lower()
+        if 'calculator' in progName:
+            progName = 'calc'
 
             # opening programs
             try:
-                os.system(fileName+'.exe')
+                os.system(progName+'.exe')
             except Exception as e:
                 print(e)
             pass
+        elif 'cmd' in progName:
+            subprocess.call('start', shell=True)
     
     # for other programs
     def openCustomPrograms(args):
