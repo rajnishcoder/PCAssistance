99cfa6b0938c80c6e01be4fc9cf08e11732a2a7c
rajnishcoder <getwere@gmail.com>
2018-02-22 08:00:37 +0530

    creating sqlite3 database

--- database/database.py
+++ database/database.py
@@ -1,8 +1,18 @@
-import MySQLdb
+import sqlite3
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="")
+class ClassName(object):
+    def __init__(self, databaseName):
+    conn = sqlite3.connect(databaseName)
+    c = conn.cursor()
 
-c = db.cursor()
+    #creating database 
+    def createTable():
+        c.execute('CREATE TABLE IF NOT EXISTS')
+        
+        
+        
+
+
 c.execute('create database if not exists')
 
 db.close()
