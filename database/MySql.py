aa0d89301ace847c94bb73121a76fe0b49dc92d5
rajnishcoder <getwere@gmail.com>
2018-03-05 07:34:47 +0530

    mysql create table, fetch, insert method done

--- database/MySql.py
+++ database/MySql.py
@@ -1,1 +1,32 @@
-import mymysql
+import pymysql
+
+class MySqlDB(object):
+    def __init__(self, command, tableName, count, key, value):
+        createTable(tableName)
+
+    
+    # db is fixed for now
+    conn = pymysql.connect(host='localhost', user='root', password='', db='pyhontest')
+    c = conn.cursor()
+    sql = ('select * from' + tableName)
+
+    # creating dynamic database 
+    def createTable(tableName):
+        global c
+        global sql
+        c.execute('CREATE TABLE IF NOT EXISTS' + tableName + '(count REAL, key TEXT, value TEXT)')
+    
+    # fetch all data
+    def fetchAll():
+        c.execute(sql)
+        return c.fetchall()
+        pass
+
+    # insert Data
+    def insertData(tableName, count, key, value):
+        c.execute('INSERT INTO' + tableName + 'VALUES('+ count +', '+ key +' '+ value +')')
+        pass 
+
+
+# MySqlDB()
+
